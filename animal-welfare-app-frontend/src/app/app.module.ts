b75c8a30ed2aeca53f6f428dd68c265009a6cea2
dpankov91 <dpankov91@gmail.com>
2021-05-07 12:10:36 +0300

    2 sockets config added

--- animal-welfare-app-frontend/src/app/app.module.ts
+++ animal-welfare-app-frontend/src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import {Injectable, NgModule} from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import {Socket, SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
 
+@Injectable()
+export class SocketAdopt extends Socket{
+  constructor() {
+    super({url: 'http://localhost:3001', options: {} });
+  }
+}
+
+@Injectable()
+export class SocketFunds extends Socket{
+  constructor() {
+    super({url: 'http://localhost:3300', options: {} });
+  }
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,9 +31,9 @@ import { FooterComponent } from './shared/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
   ],
-  providers: [],
+  providers: [SocketFunds, SocketAdopt],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
