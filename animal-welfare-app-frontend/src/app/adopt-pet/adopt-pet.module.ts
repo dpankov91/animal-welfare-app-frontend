3ee9af87017d9490cc7066bd63e1db2364dbbbf0
dpankov91 <dpankov91@gmail.com>
2021-05-10 14:17:54 +0300

    added form component for adding owner

--- animal-welfare-app-frontend/src/app/adopt-pet/adopt-pet.module.ts
+++ animal-welfare-app-frontend/src/app/adopt-pet/adopt-pet.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AdoptPetRoutingModule } from './adopt-pet-routing.module';
 import { AdoptPetComponent } from './adopt-pet.component';
+import { AdoptFormComponent } from './adopt-form/adopt-form.component';
 
 
 @NgModule({
-  declarations: [AdoptPetComponent],
+  declarations: [AdoptPetComponent, AdoptFormComponent],
   imports: [
     CommonModule,
     AdoptPetRoutingModule
