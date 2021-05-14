35f4b33f30f36d71bd18752618b6068daad95d6b
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2021-05-14 16:58:13 +0200

    initial

--- animal-welfare-app-frontend/src/app/add-pet/add-pet.component.ts
+++ animal-welfare-app-frontend/src/app/add-pet/add-pet.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Pet} from '../adopt-pet/shared/adopt-pet.model';
 import {FormBuilder} from '@angular/forms';
 import {AdoptPetService} from '../adopt-pet/shared/adopt-pet.service';
+import {Store} from '@ngxs/store';
+import {CreatePet} from '../adopt-pet/state/adopt-pet.action';
 
 @Component({
   selector: 'app-add-pet',
@@ -19,14 +21,15 @@ export class AddPetComponent implements OnInit {
   petCreate: Pet | undefined;
   error: string | undefined;
 
-  constructor(private fb: FormBuilder, private petService: AdoptPetService) { }
+  constructor(private fb: FormBuilder, private petService: AdoptPetService, private store: Store) { }
 
   ngOnInit(): void {
   }
 
-  createPet() {
+  createPet(): void {
     this.error = undefined;
     const pet: Pet = this.petForm.value;
-    this.petService.createPet(pet);
+    // this.petService.createPet(pet);
+    this.store.dispatch(new CreatePet(pet));
   }
 }
--- animal-welfare-app-frontend/src/app/adopt-pet/state/adopt-pet.state.ts
+++ animal-welfare-app-frontend/src/app/adopt-pet/state/adopt-pet.state.ts
@@ -1,17 +1,21 @@
 import {Action, Selector, State, StateContext} from '@ngxs/store';
 import {Injectable} from '@angular/core';
 import {Pet} from '../shared/adopt-pet.model';
-import {ListenForPets} from './adopt-pet.action';
+import {CreatePet, ListenForPets} from './adopt-pet.action';
 import {AdoptPetService} from '../shared/adopt-pet.service';
+
+
 
 export interface AdoptPetStateModel {
   pets: Pet[];
+
 }
 
 @State<AdoptPetStateModel>({
   name: 'AdoptPet',
   defaults: {
     pets: [],
+
   }
 })
 @Injectable()
@@ -25,6 +29,8 @@ export class AdoptPetState{
     return state.pets;
   }
 
+
+
   @Action(ListenForPets)
   getPets(ctx: StateContext<AdoptPetStateModel>): void {
     this.petService.getStatePets().subscribe(data => {
@@ -37,4 +43,48 @@ export class AdoptPetState{
     });
 
   }
+
+  @Action(CreatePet)
+  createPet({getState, setState}: StateContext<AdoptPetStateModel>, {pet}: CreatePet) {
+    console.log(pet);
+    return this.petService.createPet(pet).subscribe(data => {
+      const state = getState();
+      setState({
+        pets: [...state.pets, data]
+      });
+    });
+
+    // then((result) => {
+    //   const state = getState();
+    //   patch({
+    //     pets: [...state.pets, result]
+    //   });
+    // });
+
+    // return this.petService.createPet(pet).pipe(tap((result) => {
+    //   const state = getState();
+    //   patchState({
+    //     pets: [...state.pets, result]
+    //   });
+    // }));
+
+
+    // return this.petService.createPet(pet).pipe(tap((data) => {
+    //   const state = getState();
+    //   patchState({
+    //     pets: [...state.pets, data]
+    //   });
+    // }));
+
+
+    //   .subscribe(data => {
+    //   const state = ctx.getState();
+    //   const newState: AdoptPetStateModel = {
+    //     ... state,
+    //     pets: data
+    //   };
+    //   ctx.setState(newState);
+    // });
+  }
+
 }
